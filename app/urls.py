b94e3150f200e3a27d0e5b26b11e0f7f5925c256
Jonathan-Phiri <jonathanjaypumba@gmail.com>
2024-08-20 15:12:18 +0200

    updated

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('temperatures/', TemperatureHumidityListView.as_view(), name='temperature-list'),
     path('temperatures/<int:pk>/', TemperatureHumidityDetailView.as_view(), name='temperature-detail'),
     path('createtemperatures/', TemperatureHumidityCreateAPIView.as_view(), name='temperature-create'),
-    path('api/latest-temperature/', LatestTemperatureHumidityAPIView.as_view(), name='latest-temperature'),
+    path('api/latest_temperaturehumidity/', LatestTemperatureHumidityAPIView.as_view(), name='latest_temperaturehumidity'),
     path('temperature-monitor/', temperature_monitor, name='temperature-monitor'),
 ]
 
