4a7701e2ad1418ba59b640f1da44a68a3d35d424
Jonathan-Phiri <jonathanjaypumba@gmail.com>
2024-08-30 16:42:31 +0200

    simple

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from .models import TemperatureHumidity
 from .serializer import TemperatureHumiditySerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from datetime import datetime
 
 class TemperatureHumidityDetailView(generics.RetrieveAPIView):
     queryset = TemperatureHumidity.objects.all()
@@ -34,8 +35,14 @@ def humidity_monitor(request):
 
 
 def history_view(request):
-    readings = TemperatureHumidity.objects.all().order_by('-timestamp') 
-    return render(request, 'history.html', {'readings': readings})
+    date_query = request.GET.get('date')
+    
+    if date_query:
+        selected_date = datetime.strptime(date_query, "%Y-%m-%d").date()
+        readings = TemperatureHumidity.objects.filter(timestamp__date=selected_date).order_by('-timestamp')
+    else:
+        readings = TemperatureHumidity.objects.all().order_by('-timestamp')
+    return render(request, 'history.html', {'readings': readings, 'date_query': date_query})
 
 def save_reading(temperature, humidity):
     reading = TemperatureHumidity(temperature=temperature, humidity=humidity)
