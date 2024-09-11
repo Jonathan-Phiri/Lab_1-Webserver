11b8de040903ebeae0850550f70688afcf2bb84e
Jonathan-Phiri <jonathanjaypumba@gmail.com>
2024-09-11 12:18:29 +0200

    changes

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TemperatureHumidityDetailView,  TemperatureHumidityListView, LatestTemperatureHumidityAPIView, TemperatureHumidityCreateAPIView, TemperatureHumidityCreateAPIView, temperature_monitor, humidity_monitor, history_view
+from .views import TemperatureHumidityDetailView,  TemperatureHumidityListView, LatestTemperatureHumidityAPIView, TemperatureHumidityCreateAPIView, TemperatureHumidityCreateAPIView, temperature_monitor, humidity_monitor, history_view,  relay_control_view
 
 urlpatterns = [
     path('temperatures/', TemperatureHumidityListView.as_view(), name='temperature-list'),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('temperature-monitor/', temperature_monitor, name='temperature-monitor'),
     path('humidity-monitor/', humidity_monitor, name='humidity-monitor'),
     path('history/', history_view, name='history'),
+     path('relay-control/', relay_control_view, name='relay_control'),
 ]
 
